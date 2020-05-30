acab1862fa29d9f565a8631f66ae89203911e1cf
Mateus Mazzei Anzolin <mateus.mazzei14@gmail.com>
2020-05-30 15:54:17 +0000

    Exercicio 2 quase certo

--- pilha/ex_2_pilha.c
+++ pilha/ex_2_pilha.c
@@ -1,22 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "est-pilha.h"
-// pilha.h - implementacao dinamica sequencial de pilha
+int main (void)
+{
+  Pilha A = pilha (10);
+  Pilha B = pilha (10);
+  int x, i = 0;
 
-int main(void){
-  Pilha A = pilha(10);
-  Pilha B = pilha(10);
-  int x;
-  
   printf("Digite um numero e tecle 'enter'\nDigite '0' para terminar,vc pode inserir ate 10 numeros\n");
-  do{
-    printf("Numero: ");
-    scanf("%d",&x);
-    empilha(x, A);
-  }while(x != 0);
+  for (int j = 0; j < 10; j++) {
+      printf ("Numero: ");
+      scanf ("%d", &x);
+      empilha (x, A);
+    }
+    printf("\n----------------------------------------------------\n\n");
   
-  for(int i=0; !vaziap(A);i++){
-    if(A -> item[i] < A -> item[i-1]) {}
-  }
+  while ( (A->item[(A->topo) - i]) ){
+      if ((A->item[A->topo]) > (A->item[(A->topo) - 1]))
+	{
+	  printf ("Compara %d e %d e depois empilha em B ",(A->item[A->topo]),(A->item[(A->topo) - 1]));
+	  printf ("e depois empilha %d em B \n",(A->item[A->topo]));
+	  empilha (desempilha (A), B);
+	}
+      else{
+	  i++;
+	  printf ("Incrementou i\n");
+	  }
+    }
+  while (!vaziap (B)){
+      printf ("Pilha B: %d \n", desempilha (B));
+    }
+
   return 0;
 }
