3203283975f71781e590cce223021bd9712b4846
Mateus Mazzei Anzolin <mateus.mazzei14@gmail.com>
2020-06-26 20:01:19 -0300

    P

--- Árvore Binária/ex_silvio_2.c
+++ Árvore Binária/ex_silvio_2.c
@@ -2,16 +2,9 @@
 #include <stdlib.h>
 #include "arvore_silvio.h"
 
-int main(void) {
-   Arv I = NULL;
-   ins(7,&I);
-   ins(4,&I);
-   ins(6,&I);
-   ins(8,&I);
-   ins(2,&I);
-   ins(5,&I);
-   ins(3,&I);
-   ins(1,&I);
-   exibe(I,0);
-   return 0;
-}
+int pot(int b, int n) { int a; 
+if( n==0 ) return 1;
+ if( n==1 ) return b; a = pot(b, n/2); 
+if( n%2==0 ) return a*a; // há uma multiplicação. 
+return a*a*b; // há duas multiplicações.
+ }
